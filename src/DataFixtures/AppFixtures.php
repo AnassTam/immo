8ce57a6636c9c5fb6638f4be535ecc9ebf37da16
AnassTam <anass59000@gmail.com>
2021-04-29 11:47:10 +0200

    mise en place de easyAdmin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -30,7 +30,7 @@ class AppFixtures extends Fixture
         $user = new User();
         $user->setEmail('anass@gmail.com');
         $user->setPassword($this->passwordEncoder->encodePassword($user,'test'));
-        $user->setRoles(['Role_ADMIN']);
+        $user->setRoles(['ROLE_ADMIN']);
         $manager->persist($user);
 
 

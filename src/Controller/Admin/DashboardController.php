f41d5dcd79637b20a1089e538181963787a7d7c5
AnassTam <anass59000@gmail.com>
2021-04-30 10:42:39 +0200

    backOffice

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 
+use App\Entity\RealEstate;
 use App\Entity\Type;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -17,8 +20,16 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+    $entityManager = $this->getDoctrine();
+    $userRepository = $entityManager->getRepository(User::class);
+    $realEstateRepository = $entityManager->getRepository(RealEstate::class);
+
+        return  $this->render('admin/dashboard.html.twig',[
+            'userCount'=>$userRepository->count([]),
+            'realEstateCount' =>98
+        ]);
     }
+
 
     public function configureDashboard(): Dashboard
     {
@@ -30,5 +41,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Type::class);
+        yield MenuItem::linkToCrud('Utilisateur','fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Annonce', 'fa fa-building', RealEstate::class);
     }
 }
